a12faf2c37d296f96902921379c9e5461476a0f6
emnlvj <150634271+emnlvj@users.noreply.github.com>
2024-04-15 12:10:23 +0800

    Add files via upload

---
+++ EnrollmentSystem/EnrollmentSystem/Form1.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace EnrollmentSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SubjectCodeTextBox = new System.Windows.Forms.TextBox();
+            this.UnitsTextBox = new System.Windows.Forms.TextBox();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.OfferingComboBox = new System.Windows.Forms.ComboBox();
+            this.CourseCodeComboBox = new System.Windows.Forms.ComboBox();
+            this.CategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.CYTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SCTextBox = new System.Windows.Forms.TextBox();
+            this.PreRequisiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.CoRequisiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.CancelButotn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SubjectCodeTextBox
+            // 
+            this.SubjectCodeTextBox.Location = new System.Drawing.Point(148, 63);
+            this.SubjectCodeTextBox.Name = "SubjectCodeTextBox";
+            this.SubjectCodeTextBox.Size = new System.Drawing.Size(87, 20);
+            this.SubjectCodeTextBox.TabIndex = 0;
+            // 
+            // UnitsTextBox
+            // 
+            this.UnitsTextBox.Location = new System.Drawing.Point(148, 115);
+            this.UnitsTextBox.Name = "UnitsTextBox";
+            this.UnitsTextBox.Size = new System.Drawing.Size(87, 20);
+            this.UnitsTextBox.TabIndex = 1;
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(148, 89);
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(87, 20);
+            this.DescriptionTextBox.TabIndex = 2;
+            // 
+            // OfferingComboBox
+            // 
+            this.OfferingComboBox.FormattingEnabled = true;
+            this.OfferingComboBox.Location = new System.Drawing.Point(148, 141);
+            this.OfferingComboBox.Name = "OfferingComboBox";
+            this.OfferingComboBox.Size = new System.Drawing.Size(87, 21);
+            this.OfferingComboBox.TabIndex = 3;
+            // 
+            // CourseCodeComboBox
+            // 
+            this.CourseCodeComboBox.FormattingEnabled = true;
+            this.CourseCodeComboBox.Location = new System.Drawing.Point(150, 195);
+            this.CourseCodeComboBox.Name = "CourseCodeComboBox";
+            this.CourseCodeComboBox.Size = new System.Drawing.Size(87, 21);
+            this.CourseCodeComboBox.TabIndex = 4;
+            // 
+            // CategoryComboBox
+            // 
+            this.CategoryComboBox.FormattingEnabled = true;
+            this.CategoryComboBox.Location = new System.Drawing.Point(148, 168);
+            this.CategoryComboBox.Name = "CategoryComboBox";
+            this.CategoryComboBox.Size = new System.Drawing.Size(87, 21);
+            this.CategoryComboBox.TabIndex = 5;
+            // 
+            // CYTextBox
+            // 
+            this.CYTextBox.Location = new System.Drawing.Point(150, 222);
+            this.CYTextBox.Name = "CYTextBox";
+            this.CYTextBox.Size = new System.Drawing.Size(87, 20);
+            this.CYTextBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(65, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Subject Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(65, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(65, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Units";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(65, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Offering";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(65, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Category";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(65, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Course Code";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(65, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Curriculum Year";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Green;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(42, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(230, 277);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Subject Information";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label9.Location = new System.Drawing.Point(39, 24);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Subject Information";
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Cyan;
+            this.label10.Location = new System.Drawing.Point(292, 23);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(454, 277);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label11.Location = new System.Drawing.Point(292, 24);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(106, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Requisite Information";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label12.Location = new System.Drawing.Point(348, 63);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(71, 13);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "Subject Code";
+            // 
+            // SCTextBox
+            // 
+            this.SCTextBox.Location = new System.Drawing.Point(425, 60);
+            this.SCTextBox.Name = "SCTextBox";
+            this.SCTextBox.Size = new System.Drawing.Size(87, 20);
+            this.SCTextBox.TabIndex = 19;
+            // 
+            // PreRequisiteRadioButton
+            // 
+            this.PreRequisiteRadioButton.AutoSize = true;
+            this.PreRequisiteRadioButton.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.PreRequisiteRadioButton.Location = new System.Drawing.Point(578, 60);
+            this.PreRequisiteRadioButton.Name = "PreRequisiteRadioButton";
+            this.PreRequisiteRadioButton.Size = new System.Drawing.Size(83, 17);
+            this.PreRequisiteRadioButton.TabIndex = 20;
+            this.PreRequisiteRadioButton.TabStop = true;
+            this.PreRequisiteRadioButton.Text = "Pre-requisite";
+            this.PreRequisiteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // CoRequisiteRadioButton
+            // 
+            this.CoRequisiteRadioButton.AutoSize = true;
+            this.CoRequisiteRadioButton.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.CoRequisiteRadioButton.Location = new System.Drawing.Point(578, 83);
+            this.CoRequisiteRadioButton.Name = "CoRequisiteRadioButton";
+            this.CoRequisiteRadioButton.Size = new System.Drawing.Size(80, 17);
+            this.CoRequisiteRadioButton.TabIndex = 21;
+            this.CoRequisiteRadioButton.TabStop = true;
+            this.CoRequisiteRadioButton.Text = "Co-requisite";
+            this.CoRequisiteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(299, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(440, 157);
+            this.dataGridView1.TabIndex = 22;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Subject Code";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Description";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Units";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "C0/Pre";
+            this.Column4.Name = "Column4";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(255, 320);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(77, 22);
+            this.SaveButton.TabIndex = 23;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // CancelButotn
+            // 
+            this.CancelButotn.Location = new System.Drawing.Point(338, 320);
+            this.CancelButotn.Name = "CancelButotn";
+            this.CancelButotn.Size = new System.Drawing.Size(77, 22);
+            this.CancelButotn.TabIndex = 24;
+            this.CancelButotn.Text = "Cancel";
+            this.CancelButotn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 354);
+            this.Controls.Add(this.CancelButotn);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.CoRequisiteRadioButton);
+            this.Controls.Add(this.PreRequisiteRadioButton);
+            this.Controls.Add(this.SCTextBox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CYTextBox);
+            this.Controls.Add(this.CategoryComboBox);
+            this.Controls.Add(this.CourseCodeComboBox);
+            this.Controls.Add(this.OfferingComboBox);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.UnitsTextBox);
+            this.Controls.Add(this.SubjectCodeTextBox);
+            this.Controls.Add(this.label8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SubjectCodeTextBox;
+        private System.Windows.Forms.TextBox UnitsTextBox;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.ComboBox OfferingComboBox;
+        private System.Windows.Forms.ComboBox CourseCodeComboBox;
+        private System.Windows.Forms.ComboBox CategoryComboBox;
+        private System.Windows.Forms.TextBox CYTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox SCTextBox;
+        private System.Windows.Forms.RadioButton PreRequisiteRadioButton;
+        private System.Windows.Forms.RadioButton CoRequisiteRadioButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button CancelButotn;
+    }
+}
+
---
+++ EnrollmentSystem/EnrollmentSystem/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnrollmentSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\Server2\second semester 2023-2024\LAB802\79286_CC_APPSDEV22_1030_1230_PM_MW\79286-23217714\Desktop\Final\Jacaba.accdb";
+            OleDbConnection thisConnection = new OleDbConnection(connectionString);
+            string Ole = "Select * From SUBJECTFILE";
+            OleDbDataAdapter thisAdapter = new OleDbDataAdapter(Ole, thisConnection);
+            OleDbCommandBuilder thisBuilder = new OleDbCommandBuilder(thisAdapter);
+            DataSet thisDataSet = new DataSet();
+            thisAdapter.Fill(thisDataSet, "SubjectFile");
+
+            DataRow thisRow = thisDataSet.Tables["SubjectFile"].NewRow();
+            thisRow["SFSUBJCODE"] = Convert.ToInt16(SubjectCodeTextBox.Text);
+            thisRow["SFSUBJDESC"] = DescriptionTextBox.Text;
+            thisRow["SFSUBJUNITS"] = UnitsTextBox.Text;
+            thisRow["SFSUBJREGOFRNG"] = OfferingComboBox.Text.Substring(0, 1);
+            thisRow["SFSUBJCATEGORY"] = CategoryComboBox.Text.Substring(0, 1);
+            thisRow["SFSUBJSTATUS"] = "AC";
+            thisRow["SFSUBJCOURSECODE"] = CourseCodeComboBox.Text.Substring(0, 1);
+            thisRow["SFSUBJCURRCODE"] = CYTextBox.Text;
+
+            thisDataSet.Tables["SubjectFile"].Rows.Add(thisRow);
+            thisAdapter.Update(thisDataSet, "SubjectFile");
+
+            MessageBox.Show("Recorded");
+        }
+    }
+}
